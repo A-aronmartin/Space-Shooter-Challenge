4ae056f99660e12c1c4134c849616ab1a9b0753c
A-aronmartin <55306902+A-aronmartin@users.noreply.github.com>
2020-04-05 23:34:18 -0400

    Add files via upload

---
+++ TimeTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTrigger : MonoBehaviour
+{
+    public static float slowTimeScale = 0.5f;
+    public static float normalTimeScale = 1f;
+    public static float timeOut = 5f;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().StartCoroutine(CountDown());
+
+
+            Destroy(gameObject);
+        }
+    }
+
+    public static IEnumerator CountDown()
+    {
+        Time.timeScale = slowTimeScale;
+        yield return new WaitForSeconds(timeOut);
+        Time.timeScale = normalTimeScale;
+    }
+}
